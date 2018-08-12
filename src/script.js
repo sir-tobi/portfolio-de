d4877790223d3f761cd201d58b19b0498f470775
hoorai <tobias.schnackenberg@yahoo.com>
2018-08-12 21:03:02 +0200

    Initial Commit

---
+++ src/script.js
@@ -0,0 +1,29 @@
+window.onload = function () {
+    var allearOnScrollElements = document.querySelectorAll('.appearScroll');
+    allearOnScrollElements.forEach(element => {
+        if (isScrolledIntoView(element)) {
+            element.classList.toggle('appearScroll');
+        }
+    });
+}
+
+window.onscroll = function () {
+    var allearOnScrollElements = document.querySelectorAll('.appearScroll');
+    allearOnScrollElements.forEach(element => {
+        if (isScrolledIntoView(element)) {
+            element.classList.toggle('appearScroll');
+        }
+    });
+}
+
+function isScrolledIntoView(el) {
+    var rect = el.getBoundingClientRect();
+    var elemTop = rect.top;
+    var elemBottom = rect.bottom;
+
+    // Only completely visible elements return true:
+    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    // Partially visible elements return true:
+    //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+    return isVisible;
+}
